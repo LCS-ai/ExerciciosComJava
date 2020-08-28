7e4bc8ac559e341112935e65b4e4c388b58a0fdb
Lucas de Lucas <delucas.lucas@gmail.com>
2020-08-28 18:25:51 -0300

    comentário para teste

--- Lucas/src/main/java/Exercicio5/Arrays.java
+++ Lucas/src/main/java/Exercicio5/Arrays.java
@@ -7,6 +7,9 @@ public class Arrays
 {
     public static void main( String[] args )
     {
+        
+        //Array com linhas e colunas declaradas, array literal
+        
         int [][] notas = {
             {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 
             {10, 11, 12, 13, 14, 15, 16, 17, 18, 19}, 
